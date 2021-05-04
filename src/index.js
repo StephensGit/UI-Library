6df251f2637137fe4019bc77ea8dbce3f0fd9132
StephensGit <stephenmoran@live.ie>
2021-05-04 17:29:00 +0100

    Added Tooltips, Tabs and Dropdowns
    Added tooltips, dropdowns and tabbed content modules.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import './test.css';
+import Tooltip from './ui/tooltip'
+import Dropdown from './ui/dropdown'
+import Tabs from './ui/tabs'
 
-console.log("test");
+// create a tooltip
+const tooltipP = new Tooltip(document.querySelector('.tooltip'));
+tooltipP.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
+// create tabs 
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
